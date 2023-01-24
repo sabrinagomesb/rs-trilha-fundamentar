c9e156dffca98f028881dca22e655725e0bfad13
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2023-01-24 18:48:24 -0300

    adiciona ao exerc menu e utiliza objetos para ejs

--- node-ejs/server.js
+++ node-ejs/server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
 // informando que a ferramenta para visualizar/renderizar o html vai ser o ejs
@@ -6,11 +6,24 @@ app.set("view engine", "ejs")
 
 // criação de rota - index precisa estar dentro de uma pasta view
 app.get("/", function (req, res) {
-  res.render("index")
+  const items = [
+    { title: "D", message: "esenvolver aplicações/serviços de forma fácil" },
+    { title: "E", message: "JS usa JavaScript para renderizar HTML" },
+    { title: "M", message: "uito fácil de usar" },
+    { title: "A", message: "moriznho" },
+    { title: "I", message: "nstall EJS" },
+    { title: "S", message: "intaxe simples" },
+  ]
+  const subtitle = "Uma linguaguem de modelagem para criação de páginas HTML utilizando JavaScript."
+  res.render("pages/index", {
+    qualities: items, 
+    subtitle: subtitle,
+
+  }) // caminho do arquivo
 })
 
-app.get("/sobre", function(req, res) {
-  res.render("about")
+app.get("/sobre", function (req, res) {
+  res.render("pages/about") // caminho do arquivo
 })
 
 // função executa quando chamar a porta 8080
@@ -18,3 +31,6 @@ app.listen(8080)
 console.log("Rodando")
 
 // localhost
+
+// comunicacao request => api
+//             api =>  response
